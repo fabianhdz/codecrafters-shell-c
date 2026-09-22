35d46e0bc3151f35db824c1a977ce1b27a48bf6b
Fabian Hernandez <fabian.hernandezone@gmail.com>
2026-09-22 12:30:42 -0500

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -10,30 +10,23 @@ int main(int argc, char *argv[]) {
   setbuf(stdout, NULL);
 
   char input[MAX_SIZE];
-  while(1)
-  {
+  while(1){
+    
     printf("$ ");
   
     fgets(input, sizeof(input), stdin);
     input[strcspn(input, "\n")] = '\0';
-    int length = sizeof(input)/sizeof(input[0]);
-    char command[1024];
 
-    for(int i = 0; i < length; i++){
-      if(isspace((unsigned char)input[i])){
-        command[i] = '\0';
-        break;
-      }
-      command[i] = input[i];
-    }
+    
     if (strcmp(input, "exit") == 0){
       break;
     }
-    if (strcmp(command, "echo") == 0){
+    else if (strncmp(input, "echo ", 5) == 0){
       printf("%s\n", input + 5);
-      continue;
     }
-    printf("%s: command not found\n", input);
+    else{
+      printf("%s: command not found\n", input);
+    }
   }
 
   return 0;
