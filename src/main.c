e5ed43db5f9192102739d533fca66d50b1c33617
Fabian Hernandez <fabian.hernandezone@gmail.com>
2026-09-22 00:25:07 -0500

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -8,14 +8,16 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // TODO: Uncomment the code below to pass the first stage
-  printf("$ ");
   char command[MAX_SIZE];
+  while(1)
+  {printf("$ ");
+  
   fgets(command, sizeof(command), stdin);
   command[strcspn(command, "\n")] = '\0';
 
 
   printf("%s: command not found\n", command);
+}
 
   return 0;
 }
