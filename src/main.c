09ae57d0632ff5ee89045ebc3da7edb880d26a90
Fabian Hernandez <fabian.hernandezone@gmail.com>
2026-09-22 00:14:20 -0500

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_SIZE 4096
+
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
   // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
+  char input[MAX_SIZE];
+
+  if(scanf("%s", input) != 1){
+    printf("Invalid input");
+  }
+
+  printf("%s: command not found");
 
   return 0;
 }
