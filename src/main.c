9b2ddb4a5acca7926a26e48c6b82f287364df933
Fabian Hernandez <fabian.hernandezone@gmail.com>
2026-09-22 10:10:09 -0500

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[]) {
     command[strcspn(command, "\n")] = '\0';
 
     if (strcmp(command, "exit") == 0){
-      return 0;
+      break;
     }
     printf("%s: command not found\n", command);
   }
