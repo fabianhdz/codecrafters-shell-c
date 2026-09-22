cab1fe0f70c0125d20af803df2ae8c4858cdf0f4
Fabian Hernandez <fabian.hernandezone@gmail.com>
2026-09-22 13:16:06 -0500

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -16,26 +16,28 @@ int main(int argc, char *argv[]) {
   
     fgets(input, sizeof(input), stdin);
     input[strcspn(input, "\n")] = '\0';
+    char* builtin = strtok(input, " ");
+    char* arg = strtok(NULL, "");
+
 
     
-    if (strcmp(input, "exit") == 0){
+    if (strcmp(builtin, "exit") == 0){
       break;
     }
-    else if (strncmp(input, "echo ", 5) == 0){
+    else if (strcmp(builtin, "echo") == 0){
       printf("%s\n", input + 5);
     }
-    else if (strncmp(input, "type ", 5) == 0){
-      char *command = input + 5;
-      if(strcmp(command, "echo") == 0 || strcmp(command, "exit") == 0
-          || strcmp(command, "type") == 0){
-            printf("%s is a shell builtin\n", command);
+    else if (strcmp(builtin, "type") == 0){
+      if(strcmp(arg, "echo") == 0 || strcmp(arg, "exit") == 0
+          || strcmp(arg, "type") == 0){
+            printf("%s is a shell builtin\n", arg);
       }
       else{
-        printf("%s: not found\n", command);
+        printf("%s: not found\n", arg);
       }
     }
     else{
-      printf("%s: command not found\n", input);
+      printf("%s: command not found\n", builtin);
     }
   }
 
