14a78a9b26b51a6f5355e79927b4acc882a7e6b4
Fabian Hernandez <fabian.hernandezone@gmail.com>
2026-09-23 20:29:00 -0500

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -2,8 +2,46 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <limits.h>
+#include <unistd.h>
 
 #define MAX_SIZE 1024
+
+
+int find_path(const char* file_name, char* result, size_t result_size){
+  
+  char* path_env = getenv("PATH");
+  if(!path_env){
+    return -1;
+  }
+  // Duplicate paths so we avoid modifying original path
+  char* path = strdup(path_env);
+  if (!path){
+    return -1;
+  }
+  // Get individual path
+  char* dir = strtok(path, ":");
+
+  while (dir != NULL){
+   
+    char candidate[PATH_MAX];
+    //Attach file name to the directory path
+    snprintf(candidate, sizeof(candidate), "%s%s", candidate, file_name);
+
+      // Check that the file exists and is executable
+      if (access(candidate, X_OK) == 0){
+        snprintf(result, sizeof(result), "%s", candidate);
+        free(path);
+        return 0;
+      }
+
+    dir = strtok(NULL, ":");
+
+  }
+  free(path);
+  return -1;
+
+}
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
@@ -16,31 +54,37 @@ int main(int argc, char *argv[]) {
   
     fgets(input, sizeof(input), stdin);
     input[strcspn(input, "\n")] = '\0';
-    char* builtin = strtok(input, " ");
+    char* command = strtok(input, " ");
     char* arg = strtok(NULL, "");
 
-    if (builtin == NULL){
+    if (command == NULL){
       continue;
     }
 
     
-    if (strcmp(builtin, "exit") == 0){
+    if (strcmp(command, "exit") == 0){
       break;
     }
-    else if (strcmp(builtin, "echo") == 0){
+    else if (strcmp(command, "echo") == 0){
       printf("%s\n", input + 5);
     }
-    else if (strcmp(builtin, "type") == 0){
+    else if (strcmp(command, "type") == 0){
       if(arg != NULL && strcmp(arg, "echo") == 0 || strcmp(arg, "exit") == 0
           || strcmp(arg, "type") == 0){
             printf("%s is a shell builtin\n", arg);
       }
       else{
-        printf("%s: not found\n", arg);
+        char path[PATH_MAX];
+        if(find_path(arg, path, sizeof(path)) == 0){
+          printf("%s is %s", arg, path);
+        }
+        else{
+          printf("%s: not found\n", arg);
+        }
       }
     }
     else{
-      printf("%s: command not found\n", builtin);
+      printf("%s: command not found\n", command);
     }
   }
 
