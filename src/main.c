709df5664f1ed6542c836bbc91b4f840b672709b
Fabian Hernandez <fabian.hernandezone@gmail.com>
2026-09-23 20:40:10 -0500

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -26,7 +26,7 @@ int find_path(const char* file_name, char* result, size_t result_size){
    
     char candidate[PATH_MAX];
     //Attach file name to the directory path
-    snprintf(candidate, sizeof(candidate), "%s%s", candidate, file_name);
+    snprintf(candidate, sizeof(candidate), "%s%s", dir, file_name);
 
       // Check that the file exists and is executable
       if (access(candidate, X_OK) == 0){
