c5940f21e6919c9940a413f93a814212514dc170
Fabian Hernandez <fabian.hernandezone@gmail.com>
2026-09-25 12:17:39 -0500

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -10,124 +10,129 @@
 #define MAX_ARGS 128
 
 int find_path(const char* file_name, char* result, size_t result_size);
+int is_dir(const char* path, size_t size);
 
 int main(int argc, char* argv[]) {
-  /* Flush after every printf */
-  setbuf(stdout, NULL);
+    /* Flush after every printf */
+    setbuf(stdout, NULL);
 
-  char input[MAX_SIZE];
-  while (1) {
+    char input[MAX_SIZE];
+    while (1) {
 
-    printf("$ ");
+        printf("$ ");
 
-    fgets(input, sizeof(input), stdin);
-    input[strcspn(input, "\n")] = '\0';
+        fgets(input, sizeof(input), stdin);
+        input[strcspn(input, "\n")] = '\0';
 
-    char* input_state;
-    char* command = strtok_r(input, " ", &input_state);
-    char* arg = strtok_r(NULL, "", &input_state);
+        char* input_state;
+        char* command = strtok_r(input, " ", &input_state);
+        char* arg = strtok_r(NULL, "", &input_state);
 
-    char path[PATH_MAX];
+        char path[PATH_MAX];
 
-    if (command == NULL) {
-      continue;
-    }
-
-    if (strcmp(command, "exit") == 0) {
-      break;
-    } else if (strcmp(command, "echo") == 0) {
-      printf("%s\n", arg);
-    } else if (strcmp(command, "type") == 0) {
-      /* Builtin types */
-      if (arg != NULL &&
-        strcmp(arg, "echo") == 0 ||
-        strcmp(arg, "exit") == 0 ||
-        strcmp(arg, "type") == 0 ||
-        strcmp(arg, "pwd") == 0) {
-        printf("%s is a shell builtin\n", arg);
-      } else { /* Executable types */
-        if (arg != NULL && find_path(arg, path, sizeof(path)) == 0) {
-          printf("%s is %s\n", arg, path);
-        } else {
-          printf("%s: not found\n", arg);
+        if (command == NULL) {
+            continue;
         }
-      }
-    } else if (strcmp(command, "pwd") == 0) {
-      char cwd[PATH_MAX];
-      if (getcwd(cwd, sizeof(cwd)) != NULL) {
-        printf("%s\n", cwd);
-      }
-    } else if (strcmp(command, "cd") == 0) {
-        //TODO
+
+        if (strcmp(command, "exit") == 0) {
+            break;
+        } else if (strcmp(command, "echo") == 0) {
+            printf("%s\n", arg);
+        } else if (strcmp(command, "type") == 0) {
+            /* Builtin types */
+            if (arg != NULL &&
+              strcmp(arg, "echo") == 0 ||
+              strcmp(arg, "exit") == 0 ||
+              strcmp(arg, "type") == 0 ||
+              strcmp(arg, "pwd") == 0) {
+              printf("%s is a shell builtin\n", arg);
+          } else { /* Executable types */
+                if (arg != NULL && find_path(arg, path, sizeof(path)) == 0) {
+                    printf("%s is %s\n", arg, path);
+                } else {
+                    printf("%s: not found\n", arg);
+                }
+            }
+        } else if (strcmp(command, "pwd") == 0) {
+              char cwd[PATH_MAX];
+            if (getcwd(cwd, sizeof(cwd)) != NULL) {
+                printf("%s\n", cwd);
+            }
+        } else if (strcmp(command, "cd") == 0) {
+              //TODO
+        }
+        /* Execute command if it exists */
+        else if (find_path(command, path, sizeof(path)) == 0) {
+            char* args[MAX_ARGS];
+            args[0] = command;
+            int count = 1;
+            char* arg_state;
+            char* token = strtok_r(arg, " ", &arg_state);
+            while (token != NULL && count < MAX_ARGS - 1) {
+                args[count] = token;
+                count++;
+
+                token = strtok_r(NULL, " ", &arg_state);
+            }
+            args[count] = NULL;
+
+            /* Fork the program */
+            pid_t pid = fork();
+            switch (pid) {
+              case -1:
+                  perror("fork");
+                  continue;
+              case 0:
+                  execv(path, args);
+                  perror("execv");
+                  _exit(EXIT_FAILURE);
+              default:
+                  waitpid(pid, NULL, 0);
+                  continue;
+            }
+        } else {
+              printf("%s: command not found\n", command);
+        }
     }
-    /* Execute command if it exists */
-    else if (find_path(command, path, sizeof(path)) == 0) {
-      char* args[MAX_ARGS];
-      args[0] = command;
-      int count = 1;
-      char* arg_state;
-      char* token = strtok_r(arg, " ", &arg_state);
-      while (token != NULL && count < MAX_ARGS - 1) {
-        args[count] = token;
-        count++;
 
-        token = strtok_r(NULL, " ", &arg_state);
-      }
-      args[count] = NULL;
-
-      /* Fork the program */
-      pid_t pid = fork();
-      switch (pid) {
-        case -1:
-          perror("fork");
-          continue;
-        case 0:
-          execv(path, args);
-          perror("execv");
-          _exit(EXIT_FAILURE);
-        default:
-          waitpid(pid, NULL, 0);
-          continue;
-      }
-    } else {
-      printf("%s: command not found\n", command);
-    }
-  }
-
-  return 0;
+    return 0;
 }
 
 int find_path(const char* file_name, char* result, size_t result_size) {
 
-  char* path_env = getenv("PATH");
-  if (!path_env) {
-    return -1;
-  }
-  /* Duplicate paths so we avoid modifying original path */
-  char* path = strdup(path_env);
-  if (!path) {
-    return -1;
-  }
-  /* Get individual path */
-  char* dir = strtok(path, ":");
+    char* path_env = getenv("PATH");
+    if (!path_env) {
+        return -1;
+    }
+    /* Duplicate paths so we avoid modifying original path */
+    char* path = strdup(path_env);
+    if (!path) {
+        return -1;
+    }
+    /* Get individual path */
+    char* dir = strtok(path, ":");
 
-  while (dir != NULL) {
+    while (dir != NULL) {
 
-    char candidate[PATH_MAX];
-    /* Attach file name to the directory path */
-    snprintf(candidate, sizeof(candidate), "%s/%s", dir, file_name);
+        char candidate[PATH_MAX];
+        /* Attach file name to the directory path */
+        snprintf(candidate, sizeof(candidate), "%s/%s", dir, file_name);
 
-    /* Check that the file exists and is executable */
-    if (access(candidate, X_OK) == 0) {
-      snprintf(result, result_size, "%s", candidate);
-      free(path);
-      return 0;
+        /* Check that the file exists and is executable */
+        if (access(candidate, X_OK) == 0) {
+            snprintf(result, result_size, "%s", candidate);
+            free(path);
+            return 0;
+        }
+
+        dir = strtok(NULL, ":");
     }
 
-    dir = strtok(NULL, ":");
-  }
+    free(path);
+    return -1;
 
-  free(path);
-  return -1;
+}
+
+int is_dir(const char* path, size_t size){
 
 }
